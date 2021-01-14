8d7afb68e81c1bd13e8334ba9d4753b858ee59df
Jon Free <freejonm@gmail.com>
2021-01-14 13:58:54 -0500

    setting place as a variable so api call can be built from user input

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 console.log("script loaded");
+var place = "barcelona";
 
 $.ajax({
-    url: "https://pixabay.com/api/?key=19889884-fb56b98757db0c3c85fe41506&q=barcelona&image_type=photo&category=places",
+    url: "https://pixabay.com/api/?key=19889884-fb56b98757db0c3c85fe41506&q=" + place +"&image_type=photo&per-page=100&category=travel",
     method: "GET"
   }).then(function(response) {
     console.log(response);
@@ -12,7 +13,6 @@ $.ajax({
     for(i = 0; i < response.hits.length; i++){
         $("#images").append("<img src="+ response.hits[i].webformatURL+"></img>");
     }
-    
   });
 
   
