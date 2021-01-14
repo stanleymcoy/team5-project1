330a72ca00befd83fb144279bf786a196ed08bcc
Jon Free <freejonm@gmail.com>
2021-01-14 13:41:33 -0500

    appending images of places in barcelona using pixabay api

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
 console.log("script loaded");
+
+$.ajax({
+    url: "https://pixabay.com/api/?key=19889884-fb56b98757db0c3c85fe41506&q=barcelona&image_type=photo&category=places",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    
+    
+    console.log(response.hits[0].webformatURL);
+
+    for(i = 0; i < response.hits.length; i++){
+        $("#images").append("<img src="+ response.hits[i].webformatURL+"></img>");
+    }
+    
+  });
+
+  
