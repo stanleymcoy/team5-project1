bf839670846884f897c99bb0a957b7de4882a978
Jon Free <freejonm@gmail.com>
2021-01-16 12:45:10 -0500

    dropdown menu updates to reflect selection and produces location variable for API query, go button creates query and appends images.

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,46 @@
 console.log("script loaded");
-var place = "barcelona";
 
-$.ajax({
-    url: "https://pixabay.com/api/?key=19889884-fb56b98757db0c3c85fe41506&q=" + place +"&image_type=photo&category=places",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    
-    
-    console.log(response.hits[0].webformatURL);
+// Dropdown menu reflects selected location
+$(".dropdown-menu").on("click", "li", function(event){
+ 
+  $("#dropdownMenuButton").text(event.target.innerText);
+  console.log("You clicked", event);
 
-    for(i = 0; i < response.hits.length; i++){
+  // Go! button creates API query
+  $("#goButton").on("click", function(){
+    // setting place variable to equal value of target
+    var place = event.target.attributes[1].value;
+    console.log(place);
+
+    // API call
+    $.ajax({
+      url: "https://pixabay.com/api/?key=19889884-fb56b98757db0c3c85fe41506&q=" + place +"&image_type=photo&category=places",
+      method: "GET"
+    }).then(function(response) {
+
+      // images from query append to page
+      for(i = 0; i < response.hits.length; i++){
         $("#images").append("<img src="+ response.hits[i].webformatURL+"></img>");
     }
-  });
+
+  })
+})
+})
+
+
+// top tracks from query append to page
+
+// weather from query appends to page
+
+
+
+ 
+
+    
+    
+    
+ 
+
 
   
-console.log("push it real good");
+
